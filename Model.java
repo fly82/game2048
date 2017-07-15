f5eb9780dde89c8a6ec478981161d44a69ecb879
fly <fly82@ukr.net>
2017-07-15 16:31:59 +0200

    step 3

--- Model.java
+++ Model.java
@@ -1,12 +1,33 @@
 package com.javarush.task.task35.task3513;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Model {
     private static final int FIELD_WIDTH = 4;
     private Tile[][] gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
 
     public Model() {
+        resetGameTiles();
+    }
+
+    public List<Tile> getEmptyTiles() {
+        List<Tile> list = new ArrayList<>();
+        for (int i=0;i<FIELD_WIDTH;i++)
+            for (int j=0;j<FIELD_WIDTH;j++) if (gameTiles[i][j].value==0) list.add(gameTiles[i][j]);
+        return list;
+    }
+
+    public void resetGameTiles() {
         for (int i=0;i<FIELD_WIDTH;i++)
             for (int j=0;j<FIELD_WIDTH;j++) gameTiles[i][j]=new Tile();
+        addTile();
+        addTile();
+    }
+
+    private void addTile() {
+        List<Tile> list =getEmptyTiles();
+        list.get((int) (list.size()*Math.random())).value = Math.random() < 0.9 ? 2 : 4;
     }
 }
